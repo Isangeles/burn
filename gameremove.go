9d2d06c5cb69d8a58d328a5c7b30432f2c62ac52
Isangeles <darekapo1@gmail.com>
2019-10-24 20:03:13 +0200

    gameremove: area-character option

--- gameremove.go
+++ gameremove.go
@@ -26,7 +26,8 @@ package burn
 import (
 	"fmt"
 
-	"github.com/isangeles/flame"	
+	"github.com/isangeles/flame"
+	"github.com/isangeles/flame/core/module/object/character"
 )
 
 // gameremove handles gameremove command.
@@ -39,8 +40,56 @@ func gameremove(cmd Command) (int, string) {
 		return 2, fmt.Sprintf("%s: no option args", GameRemove)
 	}
 	switch cmd.OptionArgs()[0] {
+	case "area-character", "area-char":
+		return gameremoveAreaCharacter(cmd)
 	default:
 		return 2, fmt.Sprintf("%s: no such option: %s", GameRemove,
 			cmd.OptionArgs()[0])
 	}
+}
+
+// gameremoveAreaCharacter handles area-character option for gameremove.
+func gameremoveAreaCharacter(cmd Command) (int, string) {
+	if len(cmd.Args()) < 2 {
+		return 3, fmt.Sprintf("%s: no enought args for: %s",
+			GameAdd, cmd.OptionArgs()[0])
+	}
+	objects := make([]*character.Character, 0)
+	for _, arg := range cmd.TargetArgs() {
+		id, serial := argSerialID(arg)
+		ob := flame.Game().Module().Object(id, serial)
+		if ob == nil {
+			return 3, fmt.Sprintf("%s: object not found: %s", GameAdd, arg)
+		}
+		char, ok := ob.(*character.Character)
+		if !ok {
+			return 3, fmt.Sprintf("%s: object: %s#%s: not a character",
+				GameAdd, ob.ID(), ob.Serial())
+		}
+		objects = append(objects, char)
+	}
+	scenID := cmd.Args()[0]
+	areaID := cmd.Args()[1]
+	for _, s := range flame.Mod().Chapter().Scenarios() {
+		if s.ID() != scenID {
+			continue
+		}
+		if s.Mainarea().ID() == areaID {
+			for _, ob := range objects {
+				s.Mainarea().RemoveCharacter(ob)
+			}
+			return 0, ""
+		}
+		for _, a := range s.Mainarea().Subareas() {
+			if a.ID() != areaID {
+				continue
+			}
+			for _, ob := range objects {
+				a.RemoveCharacter(ob)
+			}
+			return 0, ""
+		}
+	}
+	return 3, fmt.Sprintf("%s: fail to found scenario area: %s: %s",
+		GameAdd, scenID, areaID)
 }
