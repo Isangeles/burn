ff54e0f31d0c157a37524396482517f7e0a41c26
Dariusz Sikora <dev@isangeles.pl>
2023-12-10 17:59:58 +0100

    tests for moduleshow

---
+++ moduleshow_test.go
@@ -0,0 +1,82 @@
+/*
+ * moduleshow_test.go
+ *
+ * Copyright 2023 Dariusz Sikora <ds@isangeles.dev>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ *
+ */
+
+package burn
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/isangeles/flame"
+	"github.com/isangeles/flame/data/res"
+)
+
+// TestModuleShowNoOptions tests handling moduleshow command
+// with no options/args.
+func TestModuleShowNoOptions(t *testing.T) {
+	Module = flame.NewModule(res.ModuleData{})
+	res, out := objectadd(testCommand{})
+	if res != 2 {
+		t.Errorf("Command result invalid: %d != 2", res)
+	}
+	expOut := fmt.Sprintf("%s: no option args", ObjectAdd)
+	if out != expOut {
+		t.Errorf("Command output invalid: '%s' != '%s'", out, expOut)
+	}
+}
+
+// TestModuleShowID tests id option for moduleshow.
+func TestModuleShowID(t *testing.T) {
+	modConf := make(map[string][]string)
+	modConf["id"] = []string{"test"}
+	Module = flame.NewModule(res.ModuleData{Config: modConf})
+	cmd := testCommand{
+		optionArgs: []string{"id"},
+	}
+	res, out := moduleshow(cmd)
+	if res != 0 {
+		t.Errorf("Command result invalid: %d != 0", res)
+	}
+	if out != Module.Conf().ID {
+		t.Errorf("Command output invalid: '%s' != '%s'", out,
+			Module.Conf().ID)
+	}
+}
+
+// TestModuleShowChapter tests chapter option for moduleshow.
+func TestModuleShowChapter(t *testing.T) {
+	modConf := make(map[string][]string)
+	modConf["chapter"] = []string{"test"}
+	Module = flame.NewModule(res.ModuleData{Config: modConf})
+	cmd := testCommand{
+		optionArgs: []string{"chapter"},
+	}
+	res, out := moduleshow(cmd)
+	if res != 0 {
+		t.Errorf("Command result invalid: %d != 0", res)
+	}
+	if out != Module.Conf().Chapter {
+		t.Errorf("Command output invalid: '%s' != '%s'", out,
+			Module.Conf().Chapter)
+	}
+}
