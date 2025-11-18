549b504de0ab5879e330b7f8c7a5a5c7a13d30bf
Dariusz Sikora <dev@isangeles.pl>
2025-11-18 23:35:33 +0100

    unused SeqReq expression type removed

--- burn.go
+++ burn.go
@@ -1,7 +1,7 @@
 /*
  * burn.go
  *
- * Copyright 2018-2022 Dariusz Sikora <dev@isangeles.pl>
+ * Copyright 2018-2025 Dariusz Sikora <ds@isangeles.dev>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -76,7 +76,6 @@ const (
 	// Expr types.
 	PipeArgExp ExpressionType = iota
 	PipeTarExp
-	SeqExp
 	NoExp
 )
 
